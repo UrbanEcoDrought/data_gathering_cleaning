d91c5faaa04e36698aab36de19bd48f978411ffa
Ross Alexander <malexander@anl.gov>
2023-03-23 15:55:39 -0500

    # 00_NOAA_GEFS_functions.R is my attempt at changing the default GEFS pulls (0.5deg) to instead pull teh 0.25 deg data. I do not know how successful it was. I was practicing with point data for now.
    gefs_datapull_test.R seems to be working fairly reliably now. Have updated to include a section where we can specify our own point. We can also make a bounding box, according to Carl.  This script outputs 3hrly forecast data. We will need to think about how we are aggregating the data to a daily level for each of the variables of interest.

--- gefs_datapull_test.R
+++ gefs_datapull_test.R
@@ -1,7 +1,7 @@
 library(gdalcubes)
 library(gefs4cast)
 # source("gefs_to_parquet.R")
-# source("gefs_functino_suite.R")
+# source("00_NOAA_GEFS_functions.R")
 
 gdalcubes_options(parallel=T) #optional
 dir.create("meow")
@@ -11,8 +11,20 @@ dir.create("meow")
 # Will want to see if we can get the 0.25 degree data
 # may require tweaking some of the functions that the 'gefs_to_parquet' function below relies upon
 
+
+# setting custom lat:lon for the morton
+# example from neon_sites: st_as_sf(sites,coords=c("longitude", "latitude"),
+#          crs = 4326)
+
+morton <- data.frame(site_name = "Morton Arboretum",
+                     site_id = "MARB",
+                     latitude = 41.8150,
+                     longitude = -88.0658)
+morton.sf <- sf::st_as_sf(morton, coords = c("longitude", "latitude"),
+                       crs=4326) |>
+  tibble::rowid_to_column("FID")
 # files I think we need to tweak are in the 'gdalcubes-methods.R fils of the gefs4cast function.
-gefs_to_parquet(dates = as.Date("2023-03-01"), ensemble = c(mean = "geavg", spr = "gespr"), "meow")
+gefs_to_parquet(dates = as.Date("2023-03-04"), ensemble = c(mean = "geavg", spr = "gespr"), "meow", sites=morton.sf)
 
 
 df <- arrow::open_dataset("meow/")
@@ -26,7 +38,7 @@ summary(as.Date(meow$reference_datetime))
 # getting unique sites.
 unique(meow$site_id)
 
-hf.dat <- meow[meow$site_id=="HARV",]
+arb.dat <- meow[meow$site_id=="MARB",]
 
 # looking at unique variables that are available in this data pull
 # Variable explanations found here: https://www.nco.ncep.noaa.gov/pmb/products/gens/geavg.t00z.pgrb2a.0p50.f003.shtml
@@ -41,3 +53,10 @@ hf.dat <- meow[meow$site_id=="HARV",]
 # "DLWRF" = "band79"), surface downward longwave radiation [W/m^2]
 
 unique(meow$variable)
+head(arb.dat)
+dim(arb.dat)
+
+
+# this has produced 3 hrly data for all of the variables that were in the primary GEFS data
+# we will need to aggregate up from the 3hrly data to daily data
+# need to determine the best way for that here.
