223aa2105a9392b6d1ecbb7555f393d583ccd22f
Ross Alexander <malexander@anl.gov>
2024-09-11 16:01:30 -0500

    Update XX_ML_ANN_modelruns.R

--- XX_ML_ANN_modelruns.R
+++ XX_ML_ANN_modelruns.R
@@ -7,7 +7,7 @@ library(terra)
 library(ggplot2)
 library(ncdf4)
 library(lubridate)
-
+library(CAST)
 
 library(keras)
 library(mlbench)
@@ -78,11 +78,58 @@ train.dat <- chi.dat3[train.index[,1],]
 test.dat <- chi.dat3[-train.index[,1],]
 
 # setting the formula for the NN
+# ran the RF model and bulling out landsat as it was not a significant variable.
 names(chi.dat3)
-# formula <- ndvi_value ~ doy + x + y + landsat.num + spi14day + spi30day + spi60day + tmax14 + tmax30 + tmax60 + tmin14 + tmin30 + tmin60
+formula <- ndvi_value ~ week.num + ndvi.lag21d + spi14day + spi30day + spi60day + spei14day + spei30day + spei60day +tmax14 + tmax30 + tmax60 + tmin14 + tmin30 + tmin60
 
-formula <- ndvi_value ~ week.num + x + y + ndvi.lag21d
+# formula <- ndvi_value ~ week.num + x + y + ndvi.lag21d
 neural_net <- neuralnet(formula, data = train.dat, hidden = c(5,3), linear.output = T)
+
+#------------------------------
+# Cast Predictive Model----
+# need to set the model up with rasters for the predictors
+
+summary(chi.dat3)
+
+formula <- ndvi_value ~ week.num + ndvi.lag21d + xy.coord + spi14day + spi30day + spi60day + 
+  spei14day + spei30day + spei60day + tmax14 + tmax30 + tmax60 + 
+  tmin14 + tmin30 + tmin60
+
+# Spatial or temporal resampling, e.g., spatial leave-location-out cross-validation (SLOO)
+folds <- CreateSpacetimeFolds(x=chi.dat, spacevar = "xy.coord", k=5)
+
+# Set up control for train function
+train_control <- trainControl(method = "cv", index = folds$index, indexOut = folds$indexOut)
+
+model <- train(formula, 
+               data = chi.dat, 
+               method = "ranger",  # Random Forest algorithm
+               trControl = train_control,
+               ntrees=100,
+               na.action="na.omit")
+
+# Print the results
+print(model)
+saveRDS(model, "processed_data/cast_model.RDS")
+# Print the results and check performance metrics
+print(model$results)
+
+# Predict NDVI on new data or using the same data
+chi.dat3$cast.pred <- predict(model, newdata = chi.dat3)
+
+CAST::global_validation(model)
+
+ggplot(data=chi.dat3) +
+  geom_point(aes(x=ndvi_value, y=cast.pred))
+
+ggplot(data=chi.dat3) + facet_wrap(year~.) +
+  geom_point(aes(x=doy, y=ndvi_value), col="black") +
+  geom_point(aes(x=doy, y=cast.pred), col="orange2")
+
+
+# Compare predicted vs actual NDVI values
+head(predictions)
+
 
 # Random Forest run----
 #Play with NDVI predictions with random forest
@@ -107,9 +154,11 @@ set.seed(123)
 randoAll<-randomForest(ndvi_value~., data=train.dat[,names(train.dat) %in% vars.rf], mtry = 6, ntree = 500)
 randoAll #90% variance explained
 
+# saving model because it took 18hrs to run
+saveRDS(randoAll, file="processed_data/randomForestmodel.RDS")
 
 AllVIP<- vip(randoAll, include_type = TRUE, horizontal = TRUE, 
-             aesthetics = list(fill = '#2a2e38'), num_features = 15) +
+             aesthetics = list(fill = '#2a2e38'), num_features = 17) +
   theme_bw() +
   theme(axis.title.x = element_blank())
 
